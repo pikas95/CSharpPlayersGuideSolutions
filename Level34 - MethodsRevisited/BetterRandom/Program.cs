abbcb28cc35b2afd631991039f1fa0549e0fec28
pikas95 <aleksejus.muravjovas@gmail.com>
2024-12-14 15:56:19 +0200

    Add Solutions of Level 34

---
+++ Level34 - MethodsRevisited/BetterRandom/Program.cs
@@ -0,0 +1,20 @@
+﻿Random random = new Random();
+Console.WriteLine(random.NextDouble(25));
+Console.WriteLine(random.NextString("up", "down", "left", "right"));
+Console.WriteLine(random.CoinFlip(0.0000001) == true ? "heads" : "tails");
+public static class RandomExtentions
+{
+    public static double NextDouble(this Random value, int upperClamp)
+    {
+        return value.NextDouble() * upperClamp;
+    }
+    public static string NextString(this Random value, params string[] choices)
+    {
+        Random length = new Random();
+        return choices[length.Next(choices.GetLength(0))];
+    }
+    public static bool CoinFlip(this Random value, double heads = 0.5)
+    {
+        return value.NextDouble() < heads ? true : false;
+    }
+}
---
+++ Level34 - MethodsRevisited/SaferNumberCrunching/Program.cs
@@ -0,0 +1,45 @@
+﻿Console.Write("Enter an integer-type number: ");
+string inputInt = Console.ReadLine()!;
+while (true)
+{
+    if (!int.TryParse(inputInt, out int result))
+    {
+        Console.Write("That's not an integer-type number. Please enter an integer-type number: ");
+        inputInt = Console.ReadLine()!;
+    }
+    else
+    {
+        Console.WriteLine(result);
+        break;
+    }
+}
+Console.Write("Enter a double-type number: ");
+string inputDouble = Console.ReadLine()!;
+while (true)
+{
+    if (!double.TryParse(inputDouble, out double result))
+    {
+        Console.Write("That's not a double-type number. Please enter an double-type number: ");
+        inputDouble = Console.ReadLine()!;
+    }
+    else
+    {
+        Console.WriteLine(result);
+        break;
+    }
+}
+Console.Write("Enter a boolean value: ");
+string inputBool = Console.ReadLine()!;
+while (true)
+{
+    if (!bool.TryParse(inputDouble, out bool result))
+    {
+        Console.Write("That's not a bool value. Please enter an bool value: ");
+        inputDouble = Console.ReadLine()!;
+    }
+    else
+    {
+        Console.WriteLine(result);
+        break;
+    }
+}
