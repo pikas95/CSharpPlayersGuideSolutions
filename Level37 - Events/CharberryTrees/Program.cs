d37b5aee7184ee2a028284f05df6c7f4edef92c1
pikas95 <aleksejus.muravjovas@gmail.com>
2024-12-15 09:13:01 +0200

    Add Solution Level 37

---
+++ Level37 - Events/CharberryTrees/Program.cs
@@ -0,0 +1,37 @@
+﻿CharberryTree tree = new CharberryTree();
+Notifier notifier = new Notifier(tree);
+Harvester harvester = new Harvester(tree);
+while (true)
+    tree.MaybeGrow();
+public class CharberryTree
+{
+    public event Action? Ripened;
+    private Random _random = new Random();
+    public bool Ripe { get; set; }
+    public void MaybeGrow()
+    {
+        // Only a tiny chance of ripening each time, but we try a lot!
+        if (_random.NextDouble() < 0.00000001 && !Ripe)
+        {
+            Ripe = true;
+            Ripened?.Invoke();
+        }
+    }
+}
+public class Notifier
+{
+    public Notifier(CharberryTree tree) { tree.Ripened += OnRipened; }
+    private void OnRipened() { Console.WriteLine("A charberry fruit has ripened!"); }
+}
+public class Harvester
+{
+    private int _harvestedCount;
+    private CharberryTree _tree;
+    public Harvester(CharberryTree tree) { _tree = tree; _tree.Ripened += OnRipened; }
+    private void OnRipened() 
+    { 
+        _harvestedCount++;  
+        _tree.Ripe = false;
+        Console.WriteLine($"Tree was harvested {_harvestedCount} {(_harvestedCount != 1 ? "times" : "time")}.");
+    }
+}
