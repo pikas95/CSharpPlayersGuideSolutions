2a6b1b12824e389efec8fb81a42ac4cd619ba540
pikas95 <aleksejus.muravjovas@gmail.com>
2024-12-15 12:33:17 +0200

    Add Solution Level 38

---
+++ Level38 - LambdaExpressions/TheLambdaSieve/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Type in operation(number): 1 - IsEven, 2 - IsPositive, 3 - MultipleOf10");
+string input = Console.ReadLine();
+Sieve sieve = input switch
+{
+    "1" => new Sieve(n => n % 2 == 0),
+    "2" => new Sieve(n => n >= 0),
+    "3" => new Sieve(n => n % 10 == n)
+};
+while (true)
+{
+    Console.Write("User, enter a number: ");
+    Console.WriteLine(sieve.IsGood(Convert.ToInt32(Console.ReadLine())));
+}
+public class Sieve
+{
+    public Func<int, bool> operation;
+    public Sieve(Func<int, bool> type) { operation = type; }
+    public bool IsGood(int number) => operation(number);
+}
