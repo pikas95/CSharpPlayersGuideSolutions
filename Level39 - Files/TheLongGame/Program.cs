dd344fac9da28770c171e48ae80d78f733d2b90e
pikas95 <aleksejus.muravjovas@gmail.com>
2024-12-15 15:07:40 +0200

    Add Solution Level 39

---
+++ Level39 - Files/TheLongGame/Program.cs
@@ -0,0 +1,11 @@
+﻿string name = Console.ReadLine()!;
+int score = 0;
+if (File.Exists($"{name}.txt"))
+    score = Convert.ToInt32(File.ReadAllText($"{name}.txt"));
+while (Console.ReadKey().Key != ConsoleKey.Enter)
+{
+    score++;
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"{name} score: {score}");
+}
+File.WriteAllText($"{name}.txt", score.ToString());
