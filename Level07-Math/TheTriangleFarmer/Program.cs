6c9e2d4a24f67cfa622975a42f7da2a29347e07d
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-12 21:17:39 +0300

    Added Level07-Math

---
+++ Level07-Math/TheTriangleFarmer/Program.cs
@@ -0,0 +1,7 @@
+﻿double tBase, tSize, tHeight;
+Console.WriteLine("What's the triangle's base, size and height?");
+tBase = Convert.ToDouble(Console.ReadLine());
+tSize = Convert.ToDouble(Console.ReadLine());
+tHeight = Convert.ToDouble(Console.ReadLine());
+double tArea = tBase * tHeight / 2;
+Console.WriteLine("Triangle's area: " + tArea);
