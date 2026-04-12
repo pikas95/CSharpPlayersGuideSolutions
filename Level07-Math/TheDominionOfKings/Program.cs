040a68bea7aa771cd9c87d11176c0df3121d3173
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-12 22:20:12 +0300

    Added another Level07 solution

---
+++ Level07-Math/TheDominionOfKings/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("How many provinces, duchies and estates you have?");
+int score = Convert.ToInt32(Console.ReadLine()) * 6; // 6 points per province
+score += Convert.ToInt32(Console.ReadLine()) * 3; // 3 points per duchie
+score += Convert.ToInt32(Console.ReadLine()); // 1 point per estate
+Console.WriteLine("Player's total score: " +  score);
