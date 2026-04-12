649914b45fafa82d28945ac66fa95cdc0e358a23
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-12 22:13:41 +0300

    Added another Level07 solution

---
+++ Level07-Math/TheFourSisterAndTheDuckbear/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("How many chocolate eggs gethered today?");
+int eggs = Convert.ToInt32(Console.ReadLine());
+int perSister = eggs / 4;
+int forDuckbear = eggs % 4;
+Console.WriteLine("Each sister gets " + perSister + ".");
+Console.WriteLine("Duckbear gets " + forDuckbear + ".");
