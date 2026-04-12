2ed3260f39bebc613f1700d3f4128d87de87dd92
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-12 14:34:31 +0300

    Added Level04-Comments

---
+++ Level04-Comments/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+string a = Console.ReadLine(); // a stores thing name
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+string b = Console.ReadLine(); // b stores description
+string c = " of Doom"; // c stores static description
+string d = "3000"; /* d stores static version of the thing */
+Console.WriteLine("The " + b + " " + a + c + " " + d + "!");
