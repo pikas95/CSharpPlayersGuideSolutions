0f357107ae94bb46f2c520d1859b73fa136d390d
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-13 17:59:29 +0300

    Added Level09-DecisionMaking

---
+++ Level09-DecisionMaking/RepairingTheClocktower/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("What's the number? ");
+if (Convert.ToInt32(Console.ReadLine()) % 2 == 0) // exceptions comes later in the book
+    Console.WriteLine("Tick");
+else
+    Console.WriteLine("Tock");
