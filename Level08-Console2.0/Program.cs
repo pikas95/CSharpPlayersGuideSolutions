d93748db33fb74a7ec3f3321f8af4b02cdfb7062
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-13 16:16:04 +0300

    Added Level08-Console2.0

---
+++ Level08-Console2.0/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Title = "Defense of Consolas";
+Console.Write("Target Row? ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Target Column? ");
+int column = Convert.ToInt32(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Deploy to:");
+Console.WriteLine($"({row}, {column - 1})");
+Console.WriteLine($"({row - 1}, {column})");
+Console.WriteLine($"({row}, {column + 1})");
+Console.WriteLine($"({row + 1}, {column})");
+Console.ForegroundColor = ConsoleColor.White;
+Console.Beep();
