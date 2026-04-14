2e5acb0917ab4930718c22c35bbcacfb2cb86d3e
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-14 16:29:37 +0300

    Added TheReplicatorOfD'To

---
+++ Level12-Arrays/TheReplicatorOfD'To/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] original = new int[5];
+Console.WriteLine("User, enter 5 integer numbers:");
+for (int i = 0; i < original.Length; i++)
+    original[i] = Convert.ToInt32(Console.ReadLine());
+
+int[] copy = new int[original.Length];
+for (int i = 0; i < copy.Length; i++)
+    copy[i] = original[i];
+
+for (int i = 0; i < copy.Length; i++)
+    Console.WriteLine($"{original[i]} {copy[i]}");
