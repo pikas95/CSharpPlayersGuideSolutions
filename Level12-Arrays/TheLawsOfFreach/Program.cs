01e05924acaf037bdfd40b4d3b56c5e4c333c007
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-04-14 16:46:28 +0300

    Added TheLawsOfFreach

---
+++ Level12-Arrays/TheLawsOfFreach/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int[] { 4, 51, -7, 13, -99, 15, -8, 45, 90 };
+int currentSmallest = int.MaxValue;
+
+foreach(int value in array)
+    if (value < currentSmallest)
+        currentSmallest = value;
+
+Console.WriteLine($"Smallest value: {currentSmallest}");
+
+int total = 0;
+for (int index = 0; index < array.Length; index++)
+    total += array[index];
+
+float average = (float)total / array.Length;
+Console.WriteLine($"Average: {average}");
