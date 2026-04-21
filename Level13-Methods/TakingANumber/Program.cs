46bae9b53e3f2b6644053184c9b0b555bade9740
AdrenAlina <aleksejus.muravjovas@gmail.com>
2026-04-21 14:24:11 +0200

    Added TakingANumber

---
+++ Level13-Methods/TakingANumber/Program.cs
@@ -0,0 +1,17 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+int AskForNumberInRange(string text, int min, int max)
+{
+    while (true)
+    {
+        Console.WriteLine(text);
+        int input = Convert.ToInt32(Console.ReadLine());
+        if (input >= min && input <= max)
+            return input;
+        Console.WriteLine("Let's try that again.");
+    }
+}
