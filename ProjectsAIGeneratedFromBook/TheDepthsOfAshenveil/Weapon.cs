96fa098836edb792ae77aec85223648668fdb28e
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-06-06 17:56:30 +0300

    Updated after Records chapter

--- ProjectsAIGeneratedFromBook/TheDepthsOfAshenveil/Weapon.cs
+++ ProjectsAIGeneratedFromBook/TheDepthsOfAshenveil/Weapon.cs
@@ -1,13 +1,1 @@
-﻿internal class Weapon
-{
-    public string Name { get; }
-    public int DamageMin { get; }
-    public int DamageMax { get; }
-
-    public Weapon(string name, int damageMin, int damageMax)
-    {
-        Name = name;
-        DamageMin = damageMin;
-        DamageMax = damageMax;
-    }
-}
+﻿internal record Weapon(string Name, int DamageMin, int DamageMax);
