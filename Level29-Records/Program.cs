7987239ad833f368b24e970e3b89db6812e01e1b
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-06-06 17:47:22 +0300

    Added WarPreparations

---
+++ Level29-Records/Program.cs
@@ -0,0 +1,12 @@
+﻿Sword basic = new(MaterialType.Iron, default, 80, 5);
+
+Sword epic = basic with { Gemstone = GemstoneType.Diamond };
+Sword legendary = basic with { Material = MaterialType.Binarium, Gemstone = GemstoneType.Bitstone };
+
+Console.WriteLine(basic);
+Console.WriteLine(epic);
+Console.WriteLine(legendary);
+
+internal record Sword(MaterialType Material, GemstoneType Gemstone, double Length, double CrossGuardWidth);
+internal enum MaterialType { Wood, Bronze, Iron, Steel, Binarium }
+internal enum GemstoneType { None, Emerald, Amber, Sapphire, Diamond, Bitstone }
