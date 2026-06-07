bb42559949bac170f00f957237c228c44e2b3d75
Aleksejus Muravjovas <aleksejus.muravjovas@gmail.com>
2026-06-07 13:59:05 +0300

    Added ColoredItems

---
+++ Level30-Generics/Program.cs
@@ -0,0 +1,29 @@
+﻿ColoredItem<Sword> blueSword = new(new Sword(), ConsoleColor.Blue);
+ColoredItem<Bow> redBow = new(new Bow(), ConsoleColor.Red);
+ColoredItem<Axe> greenAxe = new(new Axe(), ConsoleColor.Green);
+
+blueSword.Display();
+redBow.Display();
+greenAxe.Display();
+
+internal class Sword { }
+internal class Bow { }
+internal class Axe { }
+internal class ColoredItem<T> where T : notnull
+{
+    public T Item { get; }
+    public ConsoleColor Color { get; }
+
+    public ColoredItem(T item, ConsoleColor color)
+    {
+        Item = item;
+        Color = color;
+    }
+
+    public void Display()
+    {
+        Console.ForegroundColor = Color;
+        Console.WriteLine(Item.ToString());
+        Console.ResetColor();
+    }
+}
