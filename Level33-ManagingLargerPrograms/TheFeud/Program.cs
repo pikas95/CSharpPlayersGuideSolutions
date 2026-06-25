26e728e24da0409405f8f067379b341c37e71b19
AdrenAlina <aleksejus.muravjovas@gmail.com>
2026-06-25 13:57:14 +0200

    Added TheFeud

---
+++ Level33-ManagingLargerPrograms/TheFeud/Program.cs
@@ -0,0 +1,21 @@
+﻿using IField;
+using McDroid;
+using Pig = McDroid.Pig;
+
+Sheep sheep = new Sheep();
+Cow cow = new Cow();
+
+IField.Pig pigIField = new IField.Pig();
+Pig pigMcDroid = new Pig();
+
+namespace IField
+{
+    public class Sheep { }
+    public class Pig { }
+}
+
+namespace McDroid
+{
+    public class Cow { }
+    public class Pig { }
+}
